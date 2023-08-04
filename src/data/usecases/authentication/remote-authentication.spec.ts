aa733e8f3f93b38d0f29be83e4772ce30934593c
Rebeca Pacheco <rbkgarner@gmail.com>
2023-08-04 15:22:16 -0300

    test: ensure RemoteAuthentication calls the correct url

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,20 @@
+import { RemoteAuthentication } from './remote-authentication';
+
+describe('remote authentication', () => {
+  it('should call httpPostClient with correct url', async () => {
+    //
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
+
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+      }
+    }
+    const url = 'any_url';
+    const httpPostClientSpy = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+    await sut.auth();
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});
