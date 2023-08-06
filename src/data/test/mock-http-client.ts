d8518d13079917e088fd3cdc4ade4849aade4227
Rebeca Pacheco <rbkgarner@gmail.com>
2023-08-06 19:48:40 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,15 @@
 import { HttpPostParams } from '@/data/protocols/http/http-post-client';
+import { HttpResponse, HttpStatusCode } from '../protocols/http/http-response';
 export class HttpPostClientSpy implements HttpPostClientSpy {
   url?: string;
   body?: object;
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  };
 
-  async post(params: HttpPostParams): Promise<void> {
+  async post(params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url;
     this.body = params.body;
-    return Promise.resolve();
+    return Promise.resolve(this.response);
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,9 @@
 import { RemoteAuthentication } from './remote-authentication';
 import { HttpPostClientSpy } from '@/data/test/mock-http-client';
-import faker from 'faker';
 import { mockAuthentication } from '@/domain/test/mock-authentication';
+import { InvalidCredentialError } from '@/domain/errors/invalid-credential-errors';
+import { HttpStatusCode } from '@/data/protocols/http/http-response';
+import faker from 'faker';
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -30,5 +32,14 @@ describe('RemoteAuthentication', () => {
     const authenticationParams = mockAuthentication();
     await sut.auth(authenticationParams);
     expect(httpPostClientSpy.body).toEqual(authenticationParams);
+  });
+
+  it('should throw InvalidCredentialsError if HttPost returns 401', async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+    };
+    const promise = sut.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialError());
   });
 });
